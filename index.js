e7990c82eb94b4785caab45e4cdad98324002bb3
DRogalsky <dnl092@hotmail.com>
2026-04-09 16:57:21 -0700

    started login page
    started login page and process to move routes to outside file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
+
+//app.use("/", require("./routes/userRoutes.js"))
 
 app.get('/', (req, res) => {
-    res.send('RPGforum')
+    res.sendFile(path.join(__dirname, "./public/login.html"));
 })
 
 app.listen(port, () => {
